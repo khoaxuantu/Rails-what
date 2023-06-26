936ac9855d5181a6f90cac2836007592dd9a15ed
khoaxuantu <ngxktuzkai2000@gmail.com>
2023-06-26 17:35:18 +0700

    Add show users api

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def gravatar_for(user, size: 96)
+    gravatarId = Digest::MD5::hexdigest(user.email.downcase)
+    gravatarUrl = "https://secure.gravatar.com/avatar/#{gravatarId}\
+    ?s=#{size}"
+    image_tag(gravatarUrl, alt: user.name, class: "gravatar")
+  end
+
 end
