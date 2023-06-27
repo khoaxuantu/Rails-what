179363f42ed118e24fc8c054c986a5238b070193
khoaxuantu <ngxktuzkai2000@gmail.com>
2023-06-27 16:13:32 +0700

    Adding remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
-      remember user
       redirect_to user
     else
       # Return error
